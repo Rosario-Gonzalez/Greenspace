8cce1a24b08b8cc728cfd9ed8a24d81811a93d14
Rosario-Gonzalez <127342692+Rosario-Gonzalez@users.noreply.github.com>
2024-11-19 19:30:19 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,25 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    /// Simula una carga diferida
-function loadContent() {
-    const spinner = document.getElementById('spinner');
-    const content = document.getElementById('content');
-
-    // Simula un tiempo de carga (por ejemplo, 3 segundos)
-    setTimeout(() => {
-        // Oculta el spinner
+   fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(data => {
         spinner.style.display = 'none';
-        
-        // Muestra el contenido
         content.style.display = 'block';
-        content.innerHTML = `
-            <h1>¡Contenido Cargado!</h1>
-            <p>Este es el contenido que se ha cargado de forma diferida.</p>
-        `;
-    }, 3000); // 3 segundos de demora
-}
+        content.innerHTML = `<h1>${data.title}</h1><p>${data.body}</p>`;
+    });
 
-// Llama a la función cuando se cargue la página
-window.addEventListener('load', loadContent);
 
 
     // Manejo del formulario
